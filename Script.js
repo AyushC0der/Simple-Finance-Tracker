dc446525b713876e0cbc7494958ab343fdeaf505
Green <toayush0451@gmail.com>
2026-02-01 15:36:28 +0530

    adding and removing works

--- Script.js
+++ Script.js
@@ -1,36 +1,48 @@
 
 //Input
-var CatergorySel = document.getElementById("Cat_");
+var CatergorySel = document.querySelector("#Cat_");
 var type = document.querySelector("#Typ_");
+var AmountInp = document.querySelector("#Amt_");
 const IncomeCat = [
-    {text: "Salary", value: "sal"},
-    {text: "Got Money", value: "gotMon"},
-    {text: "Credit Cart Return", value: "CredRet"}
+    {text: "Salary", value: "Salary"},
+    {text: "Got Money", value: "Got Money"},
+    {text: "Credit Cart Return", value: "Credit Cart Return"}
 ];
 const ExpenCat = [
-    {text: "Food", value: "a"},
-    {text: "Chips", value: "b"},
-    {text: "Stationary", value: "c"}
+    {text: "Food", value: "Food"},
+    {text: "Chips", value: "Chips"},
+    {text: "Stationary", value: "Stationary"}
 ];
 
 //Display
+var dispAmt = document.querySelector("#displayTotalAmt_");
+var dispInc = document.querySelector("#displayTotalInc_");
+var dispExp = document.querySelector("#displayTotalExp_");
 
+var amount = 0;
+var income = 0;
+var expense = 0;
 
 //Table
 
+var tableBody = document.querySelector("#fintrack-table_ tbody");
+
+
+/////////////////////
+DisplayTotalAmount();
 
 /*Functions*/
 document.addEventListener('DOMContentLoaded', function() {
   const select = document.querySelector('#Typ_');
   select.addEventListener('change', function() {
-    if(type.value === "inc"){
+    if(type.value === "Income"){
         CatergorySel.options.length = 0;
         console.log("Income");
         IncomeCat.forEach(item => {
             CatergorySel.add(new Option(item.text, item.value));
         });
     }
-    else if(type.value === "exp"){
+    else if(type.value === "Expense"){
         CatergorySel.options.length = 0;
         console.log("Expense");
         ExpenCat.forEach(item => {
@@ -40,4 +52,26 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });   
 
+function AddElement()
+{
+    AddRow(AmountInp.value, CatergorySel.value, type.value);
+}
 
+function DisplayTotalAmount()
+{
+    dispAmt.innerHTML = `₹ ${amount}`;
+    dispInc.innerHTML = `₹ ${income}`;
+    dispExp.innerHTML = `₹ ${expense}`;
+    
+}
+
+function AddRow(amt, cat, typ)
+{
+    const row = document.createElement('tr');
+    row.innerHTML=`
+        <td>₹ ${amt}</td>
+        <td>${cat}</td>
+        <td>${typ}</td>
+    `;
+    tableBody.appendChild(row);
+}
